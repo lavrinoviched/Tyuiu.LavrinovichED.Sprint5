a9ee1e848ba667b17de8e3cbb192c9d591569e34
Tyuiu.lavrinoviched@outlook.com <Tyuiu.lavrinoviched@outlook.com>
2024-11-25 01:24:10 +0500

    Task3

---
+++ Tyuiu.LavrinovichED.Sprint5.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5; 
+
+namespace Tyuiu.LavrinovichED.Sprint5.Task3.V14.Lib
+{
+    public class DataService : ISprint5Task3V14
+    {
+        public string SaveToFileTextData(int x)
+        {
+            double calculatedResult = (4 * Math.Pow(x, 3)) / (Math.Pow(x, 3) - 1);
+
+
+            int intResult = (int)Math.Round(calculatedResult, 3);
+
+
+            string tempPath = Path.GetTempPath();
+
+
+            string filePath = Path.Combine(tempPath, "OutPutFileTask3.bin");
+
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+            {
+                writer.Write(intResult);
+            }
+
+
+            byte[] fileBytes = File.ReadAllBytes(filePath);
+            string base64String = Convert.ToBase64String(fileBytes);
+
+            return filePath;
+        }
+    }
+}
---
+++ Tyuiu.LavrinovichED.Sprint5.Task3.V14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Tyuiu.LavrinovichED.Sprint5.Task3.V14.Lib; 
+
+namespace Tyuiu.LavrinovichED.Sprint5.Task3.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Лавринович Е.Д. | АСОиУБ - 24-1";
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                                        *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                                              *");
+            Console.WriteLine("* Задание #3                                                                                       *");
+            Console.WriteLine("* Вариант #14                                                                                      *");
+            Console.WriteLine("* Выполнила: Лавринович Елизавета Дмитриевна | АСОиУБ - 24-1                                       *");
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                         *");
+            Console.WriteLine("* Дано выражение вычислить его значение при х = 3, результат сохранить в бинарный файл и вывести   *");
+            Console.WriteLine("* его на консоль. Округлить до трех знаков после запятой                                           *");
+            Console.WriteLine("*                                                                                                  *");
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                 *");
+            Console.WriteLine("****************************************************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                       *");
+            Console.WriteLine("****************************************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+
+        }
+    }
+}
